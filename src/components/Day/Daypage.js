9b9f5e9c9a8c0ff52482ccb3ef86689cb2f3df37
tongsak2543 <porramet.chi@spumail.net>
2024-10-11 15:11:41 +0700

    ok

--- src/components/Day/Daypage.js
+++ src/components/Day/Daypage.js
@@ -1,41 +1,15 @@
 import React from 'react'
 import Navbar1 from '../Navbar/navbar1'
-import formattedDate from '../function/time'
-import axios from 'axios'
-import servicesurl from '../../config'
-import { useEffect } from 'react'
 
-
-
-//     const url = servicesurl();
-//     const date = formattedDate;
-    
-
-// function Daypage() {
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 const response = await axios.get(`${url}/day/${date}`); // ดึงข้อมูลจาก API
-//                 console.log(response.data); // แสดงข้อมูลที่ได้รับ
-                
-//             }
-//              catch (error) {
-//                 console.error('Error fetching data:', error);
-//             }
-//         };
-
-//         fetchData();
-//     }, []); //ค่าวันที่เตรียงส่งไปยังกราฟทั้ง 7 วัน
-  
+function Daypage() {
   return (
     <>
     <Navbar1/>
     <div>
-      <h1>Date Is {date}</h1>
+        <h1>Welcome to the Day Page!</h1>
     </div>
     </>
   )
-// }
+}
 
 export default Daypage
